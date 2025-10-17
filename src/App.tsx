f2b4df3dc6da9dcc6d1a33c64ab3aa81b0043584
Pesquisa de Usuario Externo <pusuario@sfiec.org.br>
2025-10-17 10:27:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+  
+  return (
+    <>
+    <div className='flex flex-col lg:flex-row'>
+      <Button />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+
+
+const Button = () => {
+  return (
+    <div>
+      <button className='bg-[#1F8459] font-bold text-white px-5 py-3.5 rounded-[8px]'>Nova solicitação</button>
+    </div>
+  )
+}
+
+export default Button

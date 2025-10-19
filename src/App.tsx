7084ad87a9bf8c4fe16de4a84ac59e86418c4fea
Rodrigo <rwesley2000@gmail.com>
2025-10-19 00:17:58 -0300

    style: section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Button from './components/Button.tsx'
+import Input from './components/Input.tsx'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div className='w-screen h-screen flex flex-col items-center' >
-        <header>
+        <header className='flex-col lg:flex-row'>
           <div className='gap-2 flex items-center'>
             <span className="material-symbols-outlined text-[#1F8459]">
               redo
@@ -17,7 +18,7 @@ function App() {
             </span>
           </div>
           
-          <div className='gap-10 flex items-center'> 
+          <div className='gap-5 flex items-center'> 
             <span className='text-[#1F8459] font-semibold'>
               Solicitações de reembolso
             </span>
@@ -26,7 +27,12 @@ function App() {
               </div>
           </div>
         </header>
-        </div>
+
+        <section className='gap-6'>
+            <h1 className='text-[20px] lg:text-[24px] text-[#1F2523] font-bold'>Solicitações</h1>
+            <Input/>
+        </section>
+      </div>
     </>
   )
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,11 @@
+import ".././App.css"
+
+const Input = () => {
+  return (
+    <input placeholder='Pesquisar pelo nome' className='border-[1px] p- border-gray-300 w-[250px] lg:w-[95%] h-12 placeholder-gray-400 rounded-2xl'>
+        
+    </input>
+  )
+}
+
+export default Input

ddd5a0dd50fff822b61d0e53e213b077b654d648
Rodrigo <rwesley2000@gmail.com>
2025-10-20 15:06:00 -0300

    feat: pagination

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import Button from './components/Button.tsx'
 import Input from './components/Input.tsx'
 import RequestList from './components/RequestList.tsx'
+import Pagination from './components/pagination.tsx'
+
 
 
 function App() {
@@ -31,7 +33,7 @@ function App() {
           </div>
         </header>
 
-        <section className='flex flex-col p-6 w-[80%] h-[650px] bg-[#F9FBFA] rounded-2xl mt-[30px] gap-6'>
+        <section className='flex flex-col p-6 w-[80%] h-[700px] bg-[#F9FBFA] rounded-2xl mt-[30px] gap-6'>
             <h1 className='text-[20px] lg:text-[24px] text-[#1F2523] font-bold'>Solicitações</h1>
             <div className='flex flex-row w-[100%] gap-2.5'>
               <Input/>
@@ -41,10 +43,12 @@ function App() {
             </div>
             
             {/*linha transparentezinha */}
-            <div className='w-[90%] h-[1px] bg-[#abadac4d] self-center'></div>
+            <div className='w-[90%] h-[1px] bg-[#abadacaa] self-center'></div>
 
             <RequestList />
- 
+
+             {/* Paginação */}
+            <Pagination totalItems={15} itemsPerPage={5} />
         </section>
       </div>
     </>
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+type Props = {
+  totalItems: number;
+  itemsPerPage: number;
+  onPageChange?: (page: number) => void;
+};
+
+export default function Pagination({ totalItems, itemsPerPage, onPageChange }: Props) {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const [page, setPage] = useState(1);
+
+  const nextPage = () => {
+    if (page < totalPages) {
+      setPage(page + 1);
+      onPageChange?.(page + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+      onPageChange?.(page - 1);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center gap-3 pb-3 mt-1.5">
+      <button
+        onClick={prevPage}
+        className="bg-[#1F8459] text-white w-8 h-8 rounded-[8px] flex justify-center items-center hover:bg-[#2ac884d1] disabled:opacity-50"
+        disabled={page === 1}
+      >
+        <ChevronLeft size={18} />
+      </button>
+
+      <span className="text-gray-700 font-semibold">
+        {page}/{totalPages}
+      </span>
+
+      <button
+        onClick={nextPage}
+        className="bg-[#1F8459] text-white w-8 h-8 rounded-[8px] flex justify-center items-center hover:bg-[#2ac884d1] disabled:opacity-50"
+        disabled={page === totalPages}
+      >
+        <ChevronRight size={18} />
+      </button>
+    </div>
+  );
+}

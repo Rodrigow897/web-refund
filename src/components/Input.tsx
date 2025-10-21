40f89a91fb6ca8eeca69cfed4013dac9d88375b9
Rodrigo <rwesley2000@gmail.com>
2025-10-21 18:18:06 -0300

    feat: creating requestModal

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,11 +3,12 @@ import { twMerge } from "tailwind-merge"
 type Props = {
   className?: string
   placeholder?: string
+  disabled?: boolean
 }
 
-const Input = ({className, placeholder}: Props) => {
+const Input = ({className, placeholder, disabled}: Props) => {
   return (
-    <input placeholder={placeholder} className={twMerge('border-[1px] p-2.5 border-gray-300 w-[300px] lg:w-[95%] md:w-[90%] h-12 placeholder-gray-400 rounded-2xl outline-none', className)} >
+    <input disabled={disabled} placeholder={placeholder} className={twMerge('border-[1px] p-2.5 border-gray-300 w-[300px] lg:w-[95%] md:w-[90%] h-12 placeholder-gray-400 rounded-2xl outline-none', className)} >
         
     </input>
   )
--- src/components/RequestModal.tsx
+++ src/components/RequestModal.tsx
@@ -1,17 +1,24 @@
 import Button from "./Button"
 import Input from "./Input"
 
-const forms = () => {
+type Props = {
+    name: string;
+    category: string;
+    amount: number;
+    receipt: File | null;
+}
+
+const RequestModal = ({name, category, amount, receipt}: Props) => {
   return (
     <div className="w-100 h-110 md:w-130 md:h-128 lg:w-130 lg:h-128 bg-[#F9FBFA] rounded-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center flex flex-col">
       <header className="w-[80%] rounded-2xl bg-white flex gap-1.5 flex-col">
-        <h1 className="text-[18px] md:text-[20px] font-bold text-[#1F2523] mt-6">Nova solicitação de reembolso</h1>
+        <h1 className="text-[18px] md:text-[20px] font-bold text-[#1F2523] mt-6">Solicitação de reembolso</h1>
         <p className="text-[14px] md:text-[14px] text-[#4D5C57]">Dados da despesa para solicitar reembolso. </p>
       </header>
 
       <div className="w-[80%] mt-9 flex flex-col gap-1.5">
         <label htmlFor="" className="text-[10px] text-[#4D5C57]">NOME DA SOLICITAÇÃO</label>
-        <Input placeholder="Pesquisar pelo nome" className="w-[100%] rounded-[8px] md:w-[410px] lg:w-[414px]" />
+        <div className="w-[100%] rounded-[8px] md:w-[410px] lg:w-[414px]">{name}</div>
       </div>
 
       {/*input Categoria */}
@@ -51,4 +58,4 @@ const forms = () => {
   )
 }
 
-export default forms
+export default RequestModal

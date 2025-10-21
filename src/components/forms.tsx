141020ea12adb9333ba7ba9e70c255ffdafc8ee4
Rodrigo <rwesley2000@gmail.com>
2025-10-21 13:36:27 -0300

    feat: added button to forms

--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -3,7 +3,7 @@ import Input from "./Input"
 
 const forms = () => {
   return (
-    <div className="w-100 h-98 md:w-130 md:h-128 lg:w-130 lg:h-128 bg-[#F9FBFA] rounded-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center flex flex-col">
+    <div className="w-100 h-110 md:w-130 md:h-128 lg:w-130 lg:h-128 bg-[#F9FBFA] rounded-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center flex flex-col">
       <header className="w-[80%] rounded-2xl bg-white flex gap-1.5 flex-col">
         <h1 className="text-[18px] md:text-[20px] font-bold text-[#1F2523] mt-6">Nova solicitação de reembolso</h1>
         <p className="text-[14px] md:text-[14px] text-[#4D5C57]">Dados da despesa para solicitar reembolso. </p>
@@ -42,6 +42,11 @@ const forms = () => {
             <Button title="" className="w-[48px] h-[48px] absolute right-10" />
           </div>
         </div>
+
+        <Button
+          className="w-[80%] mt-4"
+          title="Enviar"
+        />
     </div>
   )
 }
